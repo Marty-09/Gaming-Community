3e8dd37a2c813050eabe5cf6e553da8344f60c96
Marty-09 <martina.creola2009@gmail.com>
2022-07-05 14:17:09 +0200

    Modifica files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
-client.login("OTkzNTc5ODgxMzYxMzI2MTcy.GFfq4f.J1YWrvl2cklYlSaHm19X6LLi0GdRtomyzHjFCw")
+client.login(process.env.token)
 
 client.on("ready", () => {
     console.log("Bot Online!")
