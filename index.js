b684d2c273e0b65ccb12fefd72142d91d8499b16
Marty-09 <martina.creola2009@gmail.com>
2022-07-21 16:29:15 +0200

    Parolacce

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
     if(message.content == "€risolvicubo") {
         message.channel.send("Ecco:https://media4.giphy.com/media/2CV88JdrUCOYT5Ec8j/giphy.gif")
-    }
+    }})
 
     if(message.content == "€embed") {
         var embed = new Discord.MessageEmbed()
@@ -20,9 +20,24 @@ client.on("messageCreate", (message) => {
             .setDescription(`${message.author.username} ha scritto il messaggio`)
             .setThumbnail("https://cdn.discordapp.com/attachments/990656526924533830/993788659474116689/static_3.png")
 
-    }   
-
-        message.channel.send({ embeds: [embed] })
     }
-)
 
+client.on("messageCreate", message => {
+    var parolacce = ["cazzo", "merda", "stronzo", "merda", "coglione"]
+    var trovata = false;
+
+    parolacce.forEach(parola => {
+        if(message.content.includes(parola)){
+            trovata =true;
+        }
+    })
+
+    if(trovata){
+        message.delete();
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Hai detto una parolaccia!")
+            .setDescription("Non dovevi dire queste parole ora smettila, potresti infastidire qualcuno")
+
+        message.channel.send({ embeds: [embed] })    
+    }
+})
