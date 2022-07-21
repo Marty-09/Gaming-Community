996ca972c9aaf2819cc60bd9b912179ed804b2c5
Marty-09 <martina.creola2009@gmail.com>
2022-07-21 16:52:31 +0200

    No risolviilcubo

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ client.on("ready", () => {
     console.log("Bot Online!")
 })
 
-client.on("messageCreate", (message) => {
-    if(message.content == "€risolvicubo") {
-        message.channel.send("Ecco:https://media4.giphy.com/media/2CV88JdrUCOYT5Ec8j/giphy.gif")
-    }})
-
     if(message.content == "€embed") {
         var embed = new Discord.MessageEmbed()
             .setTitle("Titolo embed")
