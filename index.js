bf208d013ad746aaea15f8df646ac2264d20e035
Marty-09 <martina.creola2009@gmail.com>
2022-08-12 10:31:36 +0200

    Eliminato sistema parolacce

--- index.js
+++ index.js
@@ -16,14 +16,18 @@ client.on("ready", () => {
             .setThumbnail("https://cdn.discordapp.com/attachments/990656526924533830/993788659474116689/static_3.png")
 
     }
-
+   
 client.on("messageCreate", message => {
-    var parolacce = ["cazzo", "merda", "stronzo", "coglione"]
+    if (message.channel.type == "DM") return
+
+    if (message.member.roles.cache.has("idRuolo1") || message.member.roles.cache.has("idRuolo2")) return
+
+    var parolacce = ["sedia", "lampada", "ciao come va"]
     var trovata = false;
     var testo = message.content;
 
     parolacce.forEach(parola => {
-        if (message.content.includes(parola)) {
+        if (message.content.toLowerCase().includes(parola.toLowerCase())) {
             trovata = true;
             testo = testo.replace(eval(`/${parola}/g`), "###");
         }
@@ -33,8 +37,8 @@ client.on("messageCreate", message => {
         message.delete();
         var embed = new Discord.MessageEmbed()
             .setTitle("Hai detto una parolaccia")
-            .setDescription("Hai scritto un messaggio con parole bloccate\rIl tuo messaggo: " + testo)
+            .setDescription("Hai scritto un messaggio con parole bloccate\rIl tuo messaggio: " + testo)
 
-        message.channel.send({ embeds: [embed] })    
+        message.channel.send({ embeds: [embed] })
     }
 })
